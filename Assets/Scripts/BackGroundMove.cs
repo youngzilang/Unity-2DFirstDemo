d5e8dba9307b15c60713310f08863ce4f487db10
3123004375-杨子朗 <1301741765@qq.com>
2026-03-12 13:21:00 +0800

    实现无尽背景

--- Assets/Scripts/BackGroundMove.cs
+++ Assets/Scripts/BackGroundMove.cs
@@ -6,22 +6,30 @@ public class BackGroundMove : MonoBehaviour
 {
     private GameObject camera;
 
-    [Header("背景跟随速度")]
+    [Header("背景跟随速度系数")]
     [SerializeField] private float backGroundMove;
 
     private float xPosition;
+    private float length;
 
     private void Start()
     {
         camera = GameObject.Find("Main Camera");
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+
 
         xPosition = transform.position.x;
     }
 
     private void Update()
     {
-        float distanceMove = camera.transform.position.x * backGroundMove;
+        float distanceMove = transform.position.x * (1 - backGroundMove);
+        float distanceMoveFllow = camera.transform.position.x * backGroundMove;
 
-        transform.position = new Vector3(xPosition + distanceMove, transform.position.y);
+        transform.position = new Vector3(xPosition + distanceMoveFllow, transform.position.y);
+
+        if (distanceMove > xPosition + length) xPosition = length + xPosition;
+        else if (distanceMove < xPosition - length) xPosition = xPosition - length;
+
     }
 }
